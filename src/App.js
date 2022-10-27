bf44716107e87039cf6712b1a38260f9e32d167b
Xuejun Du <xuejundusnow@gmail.com>
2022-10-26 21:10:32 -0400

    search city

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import Search from './components/search/search';
 
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
+
   return (
-    <div className="App">
-      <Search />
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
     </div>
   );
 }
